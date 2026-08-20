9c03c21c24ae198c6fe29f106fef66ca7f7ace99
Carl Arzadon <gitchewycide@gmail.com>
2026-08-21 01:08:45 +0800

    Update main_slave.cpp

--- src/main_slave.cpp
+++ src/main_slave.cpp
@@ -1,14 +1,3 @@
-/** +++++++++++++++++++++++++++++++++++++++++++++
- * Copyright (C) 2024 Carl Matthew Arzadon
- * All Rights Reserved 
- * 
- * Unauthorized copying of this file, via any 
- * medium is strictly prohibited. Propietary 
- * and confidential.
- * 
- * @author Carl Matthew Arzadon
- * +++++++++++++++++++++++++++++++++++++++++++ */
-
 #include <Arduino.h>
 #include <SPI.h>
 #include "RF24.h"
